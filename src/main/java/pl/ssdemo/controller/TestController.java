a7e9c2ff5d6203815b22250f3ad4b436a4e02d24
PawelJa <janecki.paw@gmail.com>
2018-04-10 11:52:16 +0200

    new user with USER_ROLE

--- src/main/java/pl/ssdemo/controller/TestController.java
+++ src/main/java/pl/ssdemo/controller/TestController.java
@@ -1,11 +1,14 @@
 package pl.ssdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.ssdemo.entity.User;
+import pl.ssdemo.entity.UserRole;
 import pl.ssdemo.repository.UserRepository;
+import pl.ssdemo.repository.UserRoleRepository;
 import pl.ssdemo.security.MyUserDetailsService;
 
 @Controller
@@ -15,7 +18,13 @@ public class TestController {
     UserRepository userRepository;
 
     @Autowired
+    UserRoleRepository userRoleRepository;
+
+    @Autowired
     MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/test")
     public String test() {
@@ -32,7 +41,26 @@ public class TestController {
         user.setUsername("Paul");
         user.setPassword("abc123");
         user.setEnabled(true);
-        user.setRole("ROLE_ADMIN");
+        UserRole userRole = new UserRole();
+        userRole.setRole("ADMIN");
+        user.setUserRole(userRole);
+        userRoleRepository.save(userRole);
+        userRepository.save(user);
+        System.out.println(user);
+        return "dodano uzytkownika";
+    }
+
+    @GetMapping("/newuser2")
+    @ResponseBody
+    public String newUser2() {
+        User user = new User();
+        user.setUsername("Paul2");
+        user.setPassword(bCryptPasswordEncoder.encode("abc1234"));
+        user.setEnabled(true);
+        UserRole userRole = new UserRole();
+        userRole.setRole("USER");
+        user.setUserRole(userRole);
+        userRoleRepository.save(userRole);
         userRepository.save(user);
         System.out.println(user);
         return "dodano uzytkownika";
